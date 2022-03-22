8c76d4b4ba5f6f44891b42fabd6da40db68d25e7
Lukasc <lukasz.r.morawski@gmail.com>
2022-03-22 21:11:56 +0100

    1. Added procedure: Get_All_monitors for allow user to select screen for display animation If screen = 0 then display in specific position 2. Rebuild settings and add many variables: timer, form, label dimensions 3. Add to form if for backing to the start position after crossing botttom of the screen

--- Animation_On_Screen.py
+++ Animation_On_Screen.py
@@ -9,62 +9,66 @@ from screeninfo import get_monitors
 
 i = 0
 form_width = 200
-form_height = 90
+form_height = 80
+pic_width = 200
+pic_height = 80
 lastClickX = 0 # used for dragging form
 lastClickY = 0 # used for dragging form
 pos_x = 0
 pos_from_file = 0
+screen_from_file = 1
 move = 0
-
-def OLD_read_Setting():
-    global pos_from_file
-    try:
-        txt_file = open('ustawienia.txt')
-        pos_from_file = txt_file.read()
-    except IOError:
-        pos_from_file = open('ustawienia.txt', 'w+') #create file
-        pos_from_file = 0
-    finally:
-        txt_file.close()
-        # print('USTAWIENIA: ', pos_from_file)
-
+timer = 500
 
 def read_Setting():
-    global pos_from_file
+    global pos_from_file, form_width, form_height, pic_width, pic_height, timer, screen_from_file
     try:
         txt_file = open('ustawienia.txt', newline='')
         reader = csv.reader(txt_file, delimiter=';')
-        #for row in reader:
-        #    print(row)
+
         for row in reader:
-            screen = row[0]
+            screen_from_file = row[0]
             pos_from_file = row[1]
+            form_width = str(row[2])
+            form_height = str(row[3])
+            pic_width = str(row[4])
+            pic_height = str(row[5])
+            timer = row[6]
     except IOError:
         pos_from_file = open('ustawienia.txt', 'w+') #create file
         pos_from_file = 0
+        screen = 1 # default - 1 screen
     finally:
         txt_file.close()
         # print('USTAWIENIA: ', pos_from_file)
+
+
 read_Setting()
 
 def Get_All_monitors():
+    global pos_from_file
+    i = 0
+    screen = []
+    screen_width = []
     for m in get_monitors():
-        print(m)
-        print((str(m).split(","))[0])
+        i =+ 1
+        print(str(m))
+        screen.append((str(m).split(","))[0].split("=")[1])
+        print(((str(m).split(","))[2].split("=")[1]))
+        screen_width.append((str(m).split(","))[2].split("=")[1])
+    if int(screen_from_file) > 0:
+        pos_from_file = int(screen[int(screen_from_file) - 1])+ int(screen_width[int(screen_from_file) - 1]) - int(form_width)
 
 
-Get_All_monitors
+
+Get_All_monitors()
+
+
 def move_up_down():
-    global move
-    global lastClickX
+    global move, lastClickX, form_width, form_height, i, timer
     screen_w = root.winfo_screenwidth()  # width of the screen
     screen_h = root.winfo_screenheight()  # height of the screen
-    global form_width
-    global form_height
 
-    global i
-    form_width = 200
-    form_height = 100
     if pos_from_file == 0:
         pos_x = screen_w - form_width
     else:
@@ -74,14 +78,15 @@ def move_up_down():
     #if lastClickX != 0:
     #    pos_x = str(lastClickX)
     #if move == 0:
-        i = i + 1
+    i = i + 1
 
-        x, y = str(pos_x), str(i)
-        loc = str(form_width) + "x" + str(form_height) + "+" + x + '+' + y
-        # loc = "300x300+" + x + '+' + y
-        root.geometry(loc)
-        root.after(500, move_up_down)  # <-- auto call to move_me again every 1/2 second
-
+    x, y = str(pos_x), str(i)
+    loc = str(form_width) + "x" + str(form_height) + "+" + x + '+' + y
+    # loc = "300x300+" + x + '+' + y
+    root.geometry(loc)
+    root.after(timer, move_up_down)  # <-- auto call
+    if i > screen_h:
+        i = 1
 
 def delete_background(image_to_transform, threshold):
     output_image = image_to_transform
@@ -158,10 +163,10 @@ def Terminate_Program(event):
 
 root = Tk()
 root.title("Lukasz Morawski")
-root.geometry("300x300+100+100")
+# root.geometry("300x300+100+100")
 image = Image.open("bus1.png")
 
-resize_image = image.resize((form_width, form_height))
+resize_image = image.resize((int(pic_width), int(pic_height)))
 ph = ImageTk.PhotoImage(resize_image)
 photo = PhotoImage(file="bus1.PNG")
 label = Label(root, image=ph, bg=from_rgb((0,0,10)))
